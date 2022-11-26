5bc70f05acbdac7ecb3fb36b793b775a45e2cfd4
Espeon196 <z3ijnn3@gmail.com>
2022-11-26 19:18:07 +0900

    first commit

---
+++ docker/model.py
@@ -0,0 +1,137 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from PIL import Image
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+import copy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+loader = transforms.Compose([
+    transforms.Resize(512 if torch.cuda.is_available() else 128),
+    transforms.ToTensor()
+])
+
+
+def image_loader(image: Image):
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+unloader = transforms.ToPILImage()
+
+
+def image_unloader(tensor):
+    image = tensor.cpu().clone()
+    image = image.squeeze(0)
+    image = unloader(image)
+
+    return image
+
+
+class ContentLoss(nn.Module):
+
+    def __init__(self, target):
+        super().__init__()
+        self.target = target.detach()
+    
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+def gram_matrix(input):
+    a, b, c, d = input.size()
+    features = input.view(a * b, c * d)
+    G = torch.mm(features, features.t())
+
+    return G.div(a * b * c * d)
+
+
+class StyleLoss(nn.Module):
+
+    def __init__(self, target_feature):
+        super().__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super().__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        return (img - self.mean) / self.std
+
+
+def get_style_model_and_losses(
+    style_img, 
+    content_img,
+    content_layers=['conv_4'],
+    style_layers=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+    ):
+    cnn = models.vgg19(pretrained=True).features.to(device).eval()
+
+    normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+    normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+    normalization = Normalization(normalization_mean, normalization_std)
+
+    content_losses = []
+    style_losses = []
+
+    model = nn.Sequential(normalization)
+
+    i = 0
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = f'conv_{i}'
+        elif isinstance(layer, nn.ReLU):
+            name = f'relu_{i}'
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = f'pool_{i}'
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = f'bn_{i}'
+        else:
+            raise RuntimeError(f'Unrecognized layer: {layer.__class__.__name__}')
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module(f'content_loss_{i}', content_loss)
+            content_losses.append(content_loss)
+        
+        if name in style_layers:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module(f"style_loss_{i}", style_loss)
+            style_losses.append(style_loss)
+
+    for i in range(len(model)-1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+            
+    model = model[:(i+1)]
+
+    return model, style_losses, content_losses
+
+
+def get_input_optimizer(input_img):
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
